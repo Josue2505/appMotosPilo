0ebdfcb8e00c107a853d183271a78052378e4cd3
Josue2505 <145162026+Josue2505@users.noreply.github.com>
2025-03-02 22:18:55 -0600

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace appMotosPilo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
