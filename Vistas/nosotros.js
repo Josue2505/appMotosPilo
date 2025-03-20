c9806a27dd12b0ed37396eb51d3c9f591dea5729
Josue2505 <jbeita@est.utn.ac.cr>
2025-03-20 15:04:03 -0600

    se cambian iconos del header nuevamente se agrega imagen footer en los servicios se realiza la vista Sobre Nosotros

---
+++ Vistas/nosotros.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const elements = document.querySelectorAll(".fade-in");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible"); // Se muestra al entrar en la vista
+        } else {
+          entry.target.classList.remove("visible"); // Se oculta al salir de la vista
+        }
+      });
+    },
+    { threshold: 0.25 }
+  ); // Se activa cuando el 20% del elemento es visible
+
+  elements.forEach((element) => observer.observe(element));
+});
